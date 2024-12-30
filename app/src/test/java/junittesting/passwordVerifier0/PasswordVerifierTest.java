1b19a8c960c6f98710a5eba434cf62ef8c504143
wonhong <cwh0099@naver.com>
2024-12-30 22:36:40 +0900

    PasswordVerifier Nested

--- app/src/test/java/junittesting/passwordVerifier0/PasswordVerifierTest.java
+++ app/src/test/java/junittesting/passwordVerifier0/PasswordVerifierTest.java
@@ -1,38 +1,86 @@
 package junittesting.passwordVerifier0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.function.Function;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class PasswordVerifierTest {
     PasswordVerifier verifier = new PasswordVerifier();
-    Function<String, VerifyResult> fakeRule = (password) -> {
-        return new VerifyResult(false, "fake reason");
-    };
     List<String> errors;
 
-    @BeforeEach
-    void addFakeRule(){
-        verifier.addRule(fakeRule);
-        errors = verifier.verifyPassword("any value");
-    }
-    
-    @Tag("testVerifyPassword")
-    @Test
-    void givenAFailingRuleReturnsErrors() {
+    @Nested
+    class WithAFailingRule {
 
-        assertTrue(errors.getFirst().contains("fake reason"));
+        @BeforeEach
+        void addFakeRule() {
+            Function<String, VerifyResult> fakeRule = (password) -> {
+                return new VerifyResult(false, "fake reason");
+            };
+            verifier.addRule(fakeRule);
+            errors = verifier.verifyPassword("any value");
+        }
+
+        @TestVerifyPassword
+        void givenAFailingRuleReturnsErrors() {
+            assertTrue(errors.getFirst().contains("fake reason"));
+        }
+
+        @Test
+        void givenAFailingRuleHasExactlyOneError() {
+            assertEquals(1, errors.size());
+        }
     }
 
-    @Tag("testVerifyPassword")
-    @Test
-    void givenAFailingRuleHasExactlyOneError() {
+    @Nested
+    class WithAPassingRule {
 
-        assertTrue(errors.size() == 1);
+        @BeforeEach
+        void addFakeRule() {
+            Function<String, VerifyResult> fakeRule = (password) -> {
+                return new VerifyResult(true, "");
+            };
+            verifier.addRule(fakeRule);
+            errors = verifier.verifyPassword("any value");
+        }
+
+        @TestVerifyPassword
+        void hasNoErrors() {
+            assertEquals(0, errors.size());
+        }
     }
+
+    @Nested
+    class WithAFailingAndPassingRule {
+
+        @BeforeEach
+        void addFakeRules() {
+            Function<String, VerifyResult> fakeRulePass = (password) -> {
+                return new VerifyResult(true, "fake success");
+            };
+            Function<String, VerifyResult> fakeRuleFail = (password) -> {
+                return new VerifyResult(false, "fake reason");
+            };
+
+            verifier.addRule(fakeRulePass);
+            verifier.addRule(fakeRuleFail);
+            errors = verifier.verifyPassword("any value");
+        }
+
+        @TestVerifyPassword
+        void hasOneError() {
+            assertEquals(1, errors.size());
+        }
+
+        @TestVerifyPassword
+        void errorTextBelongsToFailedRule() {
+            assertTrue(errors.getFirst().contains("fake reason"));
+        }
+    }
+
 }
---
+++ app/src/test/java/junittesting/passwordVerifier0/TestVerifyPassword.java
@@ -0,0 +1,17 @@
+package junittesting.passwordVerifier0;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("testVerifyPassword")
+@Test
+public @interface TestVerifyPassword {
+
+}
