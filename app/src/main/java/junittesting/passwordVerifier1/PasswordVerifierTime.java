3ed210de8641dd02a768622ca32d6c3da4639b51
wonhong <cwh0099@naver.com>
2025-01-01 22:35:46 +0900

    dayOfWeek injection by parameterization

--- app/src/main/java/junittesting/passwordVerifier1/PasswordVerifierTime.java
+++ app/src/main/java/junittesting/passwordVerifier1/PasswordVerifierTime.java
@@ -1,7 +1,6 @@
 package junittesting.passwordVerifier1;
 
 import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,10 +11,8 @@ import junittesting.passwordVerifier0.VerifyResult;
 public class PasswordVerifierTime {
     private final List<DayOfWeek> WEEKENDS = new ArrayList<>(Arrays.asList(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY));
 
-    public List<String> verifyPassword(String password, List<Function<String, VerifyResult>> rules) {
-        var dayOfWeek = LocalDate.now().getDayOfWeek();
-
-        if(WEEKENDS.contains(dayOfWeek)){
+    public List<String> verifyPassword(String password, List<Function<String, VerifyResult>> rules, DayOfWeek currentDay) {
+        if(WEEKENDS.contains(currentDay)){
             throw new RuntimeException("It's the weekend!");
         }
 
--- app/src/test/java/junittesting/passwordVerifier1/PasswordVerifierTimeTest.java
+++ app/src/test/java/junittesting/passwordVerifier1/PasswordVerifierTimeTest.java
@@ -3,24 +3,16 @@ package junittesting.passwordVerifier1;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import junittesting.passwordVerifier0.TestVerifyPassword;
 
-
 public class PasswordVerifierTimeTest {
-    private final List<DayOfWeek> WEEKENDS = new ArrayList<>(Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY));
 
     @TestVerifyPassword
     void onWeekendsThrowsExceptions() {
-        if(WEEKENDS.contains(LocalDate.now().getDayOfWeek())){
-            var varifier = new PasswordVerifierTime();
-            assertThatThrownBy(() -> varifier.verifyPassword("any value", null))
+        var varifier = new PasswordVerifierTime();
+        assertThatThrownBy(() -> varifier.verifyPassword("any value", null, DayOfWeek.SUNDAY))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("It's the weekend");
-        }
     }
 }
