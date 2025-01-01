f40a221c313495f4a2b28fa5d0f08e238586e472
wonhong <cwh0099@naver.com>
2025-01-01 22:42:15 +0900

    dayOfWeek Injection by Function

--- app/src/main/java/junittesting/passwordVerifier1/PasswordVerifierTime.java
+++ app/src/main/java/junittesting/passwordVerifier1/PasswordVerifierTime.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import junittesting.passwordVerifier0.VerifyResult;
 
 public class PasswordVerifierTime {
     private final List<DayOfWeek> WEEKENDS = new ArrayList<>(Arrays.asList(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY));
 
-    public List<String> verifyPassword(String password, List<Function<String, VerifyResult>> rules, DayOfWeek currentDay) {
-        if(WEEKENDS.contains(currentDay)){
+    public List<String> verifyPassword(String password, List<Function<String, VerifyResult>> rules, Supplier<DayOfWeek> getDaySupplier) {
+        if(WEEKENDS.contains(getDaySupplier.get())){
             throw new RuntimeException("It's the weekend!");
         }
 
--- app/src/test/java/junittesting/passwordVerifier1/PasswordVerifierTimeTest.java
+++ app/src/test/java/junittesting/passwordVerifier1/PasswordVerifierTimeTest.java
@@ -3,6 +3,7 @@ package junittesting.passwordVerifier1;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.DayOfWeek;
+import java.util.function.Supplier;
 
 import junittesting.passwordVerifier0.TestVerifyPassword;
 
@@ -11,7 +12,8 @@ public class PasswordVerifierTimeTest {
     @TestVerifyPassword
     void onWeekendsThrowsExceptions() {
         var varifier = new PasswordVerifierTime();
-        assertThatThrownBy(() -> varifier.verifyPassword("any value", null, DayOfWeek.SUNDAY))
+        Supplier<DayOfWeek> alwaysSunday = () -> DayOfWeek.SUNDAY;
+        assertThatThrownBy(() -> varifier.verifyPassword("any value", null, alwaysSunday))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("It's the weekend");
     }
